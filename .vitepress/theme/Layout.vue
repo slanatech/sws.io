aa623b96100b60a8a0e68e9a624db43d184b6637
sv2 <sv2@slana.tech>
2022-03-04 12:52:17 -0800

    Blog - initial

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,10 +1,11 @@
 <template>
-  <Home v-if="isIndex" />
+  <Blog v-if="isIndex" />
   <PageLayout v-else />
 </template>
 
 <script>
 import Home from './Home.vue'
+import Blog from './Blog.vue'
 import PageLayout from './PageLayout.vue'
 import { useRoute, useData } from 'vitepress'
 import { ref, watch, computed } from 'vue';
@@ -12,10 +13,15 @@ import { ref, watch, computed } from 'vue';
 //const { site, page, theme, frontmatter } = useData()
 
 export default {
-  components: { Home, PageLayout },
+  components: { Home, Blog, PageLayout },
   setup() {
     //const { site, page, theme } = useData();
     const route = useRoute();
+    // TODO return component based on current route:
+    //  Home
+    //  Blog
+    //  PageLayout
+    //  BlogLayout
     return {
       isIndex: computed(() => route.data.relativePath  === 'index.md')
     };
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,7 +1,13 @@
 import './tailwind.css'
 import './prism.css'
 import Layout from './Layout.vue'
+import Blog from './Blog.vue'
 
 export default {
   Layout,
+  enhanceApp({ app, router, siteData }) {
+    // app is the Vue 3 app instance from `createApp()`. router is VitePress'
+    // custom router. `siteData` is a `ref` of current site-level metadata.
+    //router.addRoutes({ path: '/blog', component: Blog })
+  }
 }
